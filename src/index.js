dd1ffa78356ba1ca32e4bf0b3c8eb940ffd0dcd6
Phil <phil.github@gmail.com>
2024-04-08 14:54:59 +0300

    added delete button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 const BASE_URL = "http://localhost:3000/films";
 // This Function updates the content on the Page
 function updatePageInfo(movieName) {
+  // Updates the Image based on the movie picked
   document.getElementById("poster").src = movieName.poster;
+  document.getElementById("poster").alt = movieName.title;
+  // Updates the Title based on the movie picked
   document.getElementById("title").textContent = movieName.title;
+  // Updates the runTime based on the movie picked
   document.getElementById(
     "runtime"
   ).textContent = `${movieName.runtime} minutes`;
+  // Updates the filmInfo based on the movie picked
   document.getElementById("film-info").textContent = movieName.description;
+  // Updates the showtime based on the movie picked
   document.getElementById("showtime").textContent = movieName.showtime;
+  //  Shows the ticket number available based on the movie picked
   document.getElementById("ticket-num").textContent = movieName.tickets_sold;
+
+  const extraContent = document.querySelector("#delete-button");
+  extraContent.innerText = "Delete";
+  extraContent.className = "ui orange button";
+
+  extraContent.addEventListener("click", () => {
+    removeMovie(movieName.id);
+  });
+
+  function removeMovie(movieId) {
+    fetch(`http://localhost:3000/films/${movieId}`, {
+      method: "DELETE",
+    });
+  }
 }
 fetch(BASE_URL)
   .then((response) => response.json())
